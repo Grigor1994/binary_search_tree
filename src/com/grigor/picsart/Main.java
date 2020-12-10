59fed93d432a044083b9007afcbb848fb5805ded
Grigor1994 <grigor.avetisyan@hotmail.com>
2020-12-10 15:13:02 +0400

    implement inOrder method.

--- src/com/grigor/picsart/Main.java
+++ src/com/grigor/picsart/Main.java
@@ -13,6 +13,8 @@ public class Main {
         System.out.println(tree.contains(4));
         tree.add(4);
         System.out.println(tree.contains(4));
+        tree.add(42);
+        tree.add(3);
         tree.inOrder();
     }
 }
